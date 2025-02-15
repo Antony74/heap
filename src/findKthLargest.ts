3ed319d8a00481c1b0fd083ec26fb35e7d301870
Antony74 <akb@akb.me.uk>
2025-02-15 21:30:05 +0000

    Get this working with a mock heap

--- src/findKthLargest.ts
+++ src/findKthLargest.ts
@@ -1,7 +1,20 @@
 import { createHeap } from './heap';
 
+const createMockHeap = () => {
+    const arr: number[] = [];
+
+    return {
+        push: (value: number) => {
+            arr.push(value);
+            arr.sort();
+        },
+        pop: () => arr.shift(),
+        size: () => arr.length
+    };
+};
+
 export const findKthLargest = (nums: number[], k: number): number => {
-    const heap = createHeap();
+    const heap = createMockHeap();
     for (const num of nums) {
         heap.push(num);
         if (heap.size() > k) {
--- test/findKthLargest.test.ts
+++ test/findKthLargest.test.ts
@@ -1,6 +1,8 @@
 import { findKthLargest } from '../src/findKthLargest';
 
-describe('Case 1', () => {
-    const result = findKthLargest([3, 2, 1, 5, 6, 4], 2);
-    expect(result).toEqual(5);
+describe('findKthLargest', () => {
+    test('Case 1', () => {
+        const result = findKthLargest([3, 2, 1, 5, 6, 4], 2);
+        expect(result).toEqual(5);
+    });
 });
